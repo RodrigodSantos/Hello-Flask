978b2c6d4481c58b3e8f85ea5eaa24b593c10792
Rodrigo dos Santos <94724162+RodrigodSantos@users.noreply.github.com>
2023-04-24 09:52:02 -0300

    ...

--- model/pessoa.py
+++ model/pessoa.py
@@ -1,16 +1,18 @@
 from flask_restful import fields
 from database import db
 
-pessoa_field = {"id": fields.Integer, "nome": fields.String, "email": fields.String}
+pessoa_field = {"id": fields.Integer, "nome": fields.String, "email": fields.String, "status": fields.String}
 
 class Pessoa(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     nome = db.Column(db.String, nullable=False)
     email = db.Column(db.String, unique=True, nullable=False)
+    status = db.Column(db.String, nullable=False)
     
-    def __init__(self, nome, email):
+    def __init__(self, nome, email, status):
       self.nome = nome
       self.email = email
+      self.status = "activated"
       
     def __repr__(self):
         return f'<Pessoa {self.nome}>'      
